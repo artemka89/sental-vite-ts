7b0e0a477ab227fbe830a82290664b1cecff23e4
Moshniaga Artem <artem.m89@mail.ru>
2024-09-30 13:42:50 +0300

    feat: completed types task

--- src/app/main.ts
+++ src/app/main.ts
@@ -5,6 +5,27 @@ import "./style/normalize.css";
 import "./style//variables.css";
 import "./style/global.css";
 
+// Задача на типизацию ------------
+export type DeepReadonly<T> = {
+  readonly [P in keyof T]: T[P] extends (infer U)[]
+    ? DeepReadonly<U>[]
+    : T[P] extends object
+    ? DeepReadonly<T[P]>
+    : T[P];
+};
+
+interface Example {
+  foo: string;
+  bar: number[];
+  baz: {
+    qux: string;
+  };
+}
+
+export type ReadonlyExample = DeepReadonly<Example>;
+
+// --------------------------------
+
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 createRouter(routes, app);
